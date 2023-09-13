13f771124f2e05282d97b8ec87a8e4493a33dc24
Oge <52808493+ogeitunu@users.noreply.github.com>
2023-09-13 08:10:13 +0100

    Pfeat

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-function updateDateAndTime() {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const currentDate = new Date();
-    const currentDay = days[currentDate.getUTCDay()];
-    const currentUTCTime = currentDate.getTime();
+const elementCurrenctDay = document.querySelector("#currentDayOfTheWeek")
+const elementCurrentUTCTime = document.querySelector("#currentUTCTime");
 
-    document.querySelector( 
-        '[data-testid="currentDayOfTheWeek"]') .textContent = `{currentDay}`;
-        document.querySelector('[data-testid="currentUTCTime "]').textContent = `${currentUTCTime}`;
+const currentDate = new Date();
+
+function getCurrentDayOfTheWeek() {
+    const daysOfWeek = ["Sunday", 
+"Monday", "Tuesday", "Wednesday", 
+"Thursaday", "Friday", "Saturday"];
+    const currentDayOfTheWeek = daysOfWeek[currentDate.getDay()];
+    return currentDayOfTheWeek;
 }
 
-// update time and date every second
-setInterval(updateDateAndTime, 1000);
+elementCurrenctDay.textContent = getCurrentDayOfTheWeek();
+
+setInterval(() => {
+    const currentDate = new Date();
+    const currentUTCTime = currentDate.getTime();
+
+    elementCurrentUTCTime.textContent = currentUTCTime
+    
+}, 1000);
