b417dc885539f791d4b14ad5253aacb1f781021f
Oge <52808493+ogeitunu@users.noreply.github.com>
2023-09-15 09:55:09 +0100

    new feat

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-const elementCurrenctDay = document.querySelector("#currentDayOfTheWeek")
+// Get the HTML elements
+const elementCurrentDay = document.querySelector("#currentDayOfTheWeek");
 const elementCurrentUTCTime = document.querySelector("#currentUTCTime");
 
+// Create a Date object for the current date and time
 const currentDate = new Date();
 
+// Function to get the current day of the week
 function getCurrentDayOfTheWeek() {
-    const daysOfWeek = ["Sunday", 
-"Monday", "Tuesday", "Wednesday", 
-"Thursaday", "Friday", "Saturday"];
+    const daysOfWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
     const currentDayOfTheWeek = daysOfWeek[currentDate.getDay()];
     return currentDayOfTheWeek;
 }
 
-elementCurrenctDay.textContent = getCurrentDayOfTheWeek();
+// Set the text content of elementCurrentDay to the current day of the week
+elementCurrentDay.textContent = getCurrentDayOfTheWeek();
 
+// Update the current UTC time every second
 setInterval(() => {
     const currentDate = new Date();
     const currentUTCTime = currentDate.getTime();
 
-    elementCurrentUTCTime.textContent = currentUTCTime
-    
+    // Set the text content of elementCurrentUTCTime to the current UTC time
+    elementCurrentUTCTime.textContent = currentUTCTime;
 }, 1000);
